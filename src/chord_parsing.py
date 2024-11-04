d22fbf86daa65d49bddfc2c0461b111f668cefcb
jaroncui <jaron.cui@gmail.com>
2024-11-04 17:32:39 -0500

    started testing synthetic training music generation

---
+++ src/chord_parsing.py
@@ -0,0 +1,16 @@
+from musiclang import Score
+from musiclang.library import *
+
+score = Score.from_midi('pull_files/midi_files/bwv772.mid')
+chords = score.to_chords_with_duration()
+print(chords)
+notes = r
+for chord in chords:
+  # chord.ton
+  # arpeggio = chord.chord_notes[0].e
+  # for note in chord.chord_notes[1:]:
+  #   arpeggio += note.e
+  # notes += arpeggio
+  notes += chord.to_voicing()
+
+notes.to_midi('test.mid')
